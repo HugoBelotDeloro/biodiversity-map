a7ba11c8c0aef68d349f04bae0a48055585cadb9
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2022-05-14 13:07:21 +0200

    Walk phylogenetic tree

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -8,4 +8,28 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoiaHVnb2JkIiwiYSI6ImNsMms4aHlwNDAxMzIzaW5rN3ZvNmFna3UifQ.nyfKWO_oFrNM_5Vd1NGI6g'
 }).addTo(map);
 
-fetch('/fetchDataset?prefix=zika').then(res => console.log('res', res)).catch(err => console.log('err', err));
+function walkCountries(tree, sumPerCountry) {
+    let confidencePerCountry = tree.node_attrs.country.confidence;
+    for (const country in confidencePerCountry) {
+        if (!sumPerCountry[country]) {
+            sumPerCountry[country] = 0;
+        }
+        sumPerCountry[country] += confidencePerCountry[country];
+    }
+    if (tree.children) {
+        for (const child of tree.children) {
+            walkCountries(child, sumPerCountry);
+        }
+    }
+}
+
+function processTree(tree) {
+    const sumPerCountry = {};
+    walkCountries(tree, sumPerCountry);
+    console.log(sumPerCountry);
+}
+
+fetch('/fetchDataset?prefix=zika')
+    .then(res => res.json())
+    .then(res => processTree(res.tree))
+    .catch(err => console.log('err', err));
