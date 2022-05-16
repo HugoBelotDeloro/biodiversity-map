9e877547e30e46a76a14906e4cadf3e977261de3
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2022-05-16 19:50:58 +0200

    Display map info

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,11 +1,13 @@
-let map = L.map('map').setView([51.505, -0.09], 13);
+const accessToken = 'pk.eyJ1IjoiaHVnb2JkIiwiYSI6ImNsMms4aHlwNDAxMzIzaW5rN3ZvNmFna3UifQ.nyfKWO_oFrNM_5Vd1NGI6g';
+
+let map = L.map('map').setView([0, 0], 2);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiaHVnb2JkIiwiYSI6ImNsMms4aHlwNDAxMzIzaW5rN3ZvNmFna3UifQ.nyfKWO_oFrNM_5Vd1NGI6g'
+    accessToken
 }).addTo(map);
 
 function walkCountries(tree, sumPerCountry) {
@@ -23,13 +25,42 @@ function walkCountries(tree, sumPerCountry) {
     }
 }
 
-function processTree(tree) {
+async function getCountryCoordinates(country) {
+  return fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${country}.json?access_token=${accessToken}`)
+      .then(res => res.json())
+      .then(res => {
+        for (const feature of res.features) {
+          if (feature.place_type[0] === 'country') {
+            return feature.center;
+          }
+        }
+        return null;
+      })
+      .catch(err => console.log(`Error fetching coordinates for ${country}`, err));
+}
+
+async function processTree(tree) {
     const sumPerCountry = {};
     walkCountries(tree, sumPerCountry);
-    console.log(sumPerCountry);
+    const coordinatesPerCountry = {};
+    for (const country of Object.keys(sumPerCountry)) {
+      const coords = await getCountryCoordinates(country)
+      if (coords === null) {
+        console.log(`Unknown country ${country}`);
+        continue;
+      }
+      coordinatesPerCountry[country] = coords;
+      L.circle([coords[1], coords[0]], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 50000 * Math.sqrt(sumPerCountry[country] / Math.PI)
+      }).addTo(map);
+    }
 }
 
 fetch('/fetchDataset?prefix=zika')
     .then(res => res.json())
+    .catch(err => console.log('Error fetching dataset', err))
     .then(res => processTree(res.tree))
-    .catch(err => console.log('err', err));
+    .catch(err => console.log('Error processing tree', err));
