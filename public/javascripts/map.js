bd30af92d4f6c98f82c8446368491b9813b9779e
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2022-05-24 18:14:00 +0200

    Use dataset's geo_resolutions instead of mapbox API

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -13,49 +13,59 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken
 }).addTo(map);
 
-function walkCountries(tree, sumPerCountry) {
-  let confidencePerCountry = tree.node_attrs.country?.confidence ||
-    tree.node_attrs.region?.confidence ||
-    tree.node_attrs.state?.confidence;
-  for (const country in confidencePerCountry) {
-    if (!sumPerCountry[country]) {
-      sumPerCountry[country] = 0;
-    }
-    sumPerCountry[country] += confidencePerCountry[country];
-  }
+function walkTree(tree, callback) {
+  callback(tree);
   if (tree.children) {
     for (const child of tree.children) {
-      walkCountries(child, sumPerCountry);
+      walkTree(child, callback);
     }
   }
 }
 
-async function getCountryCoordinates(country) {
-  return fetch(`/getGeocode?access_token=${accessToken}&country=${country}`)
-      .then(res => res.json())
-      .then(res => {
-        for (const feature of res.features) {
-          if (feature.place_type[0] === 'country') {
-            return feature.center;
-          }
-        }
-        return null;
-      })
-      .catch(err => console.log(`Error fetching coordinates for ${country}`, err));
+function getCoordinates(name, key, geo_resolutions) {
+  for (const region_data of geo_resolutions) {
+    if (region_data.key === key) {
+      if (region_data.demes[name]) {
+        const { latitude, longitude } = region_data.demes[name];
+        return [ latitude, longitude];
+      }
+      console.log(`Error: no ${key} named ${name}`);
+      return undefined;
+    }
+  }
+  console.log(`Error: unknown geo_resolution key ${key}`);
+  return undefined;
 }
 
-async function processTree(tree, color, fillColor) {
+async function processTree(data, color, fillColor) {
     const sumPerCountry = {};
-    walkCountries(tree, sumPerCountry);
-    const coordinatesPerCountry = {};
-    for (const country of Object.keys(sumPerCountry)) {
-      const coords = await getCountryCoordinates(country)
-      if (coords === null) {
-        console.log(`Unknown country ${country}`);
-        continue;
+    const coordsPerCountry = {};
+    walkTree(data.tree, (tree) => {
+      let key = null;
+      if (tree.node_attrs.country) {
+        key = 'country';
+      } else if (tree.node_attrs.region) {
+        key = 'region';
+      } else if (tree.node_attrs.state) {
+        key = 'state';
+      } else {
+        console.log('Error: no geo_resolution information for node ', tree);
       }
-      coordinatesPerCountry[country] = coords;
-      L.circle([coords[1], coords[0]], {
+      let confidencePerCountry = tree.node_attrs[key].confidence;
+      for (const country in confidencePerCountry) {
+        if (!sumPerCountry[country]) {
+          sumPerCountry[country] = 0;
+        }
+        sumPerCountry[country] += confidencePerCountry[country];
+        if (!coordsPerCountry[country]) {
+          coordsPerCountry[country] = getCoordinates(country, key, data.meta.geo_resolutions)
+        }
+      }
+
+    });
+    for (const country of Object.keys(sumPerCountry)) {
+      const coords = coordsPerCountry[country];
+      L.circle(coords, {
         color,
         fillColor,
         fillOpacity: 0.5,
@@ -68,7 +78,7 @@ function loadDataset(dataset, color, fillColor) {
   fetch(`/fetchDataset?prefix=${dataset}`)
   .then(res => res.json())
   .catch(err => console.log(`Error fetching dataset ${dataset}`, err))
-  .then(res => processTree(res.tree, color, fillColor))
+  .then(res => processTree(res, color, fillColor))
   .catch(err => console.log(`Error processing tree ${dataset}`, err));
 }
 
